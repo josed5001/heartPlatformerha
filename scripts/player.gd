d337f36742c911935f0c5f133d7f9d48ad251275
josed5001 <122053779+josed5001@users.noreply.github.com>
2023-12-12 12:42:10 -0800

    part 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 
 func _physics_process(delta):
@@ -20,7 +21,11 @@ func _physics_process(delta):
 	handle_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animations(input_axis)
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
 
 func apply_gravity(delta):
 	if not is_on_floor():
@@ -28,10 +33,10 @@ func apply_gravity(delta):
 
 func handle_jump():
 	# Handle Jump.
-	if is_on_floor():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = JUMP_VELOCITY
-	else:
+	if not is_on_floor():
 		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY /2:
 			velocity.y = JUMP_VELOCITY /2
 			
