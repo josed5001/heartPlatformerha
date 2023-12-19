9628cfc2be2d2e9ae3b66346bc50ce84b901bf5d
josed5001 <122053779+josed5001@users.noreply.github.com>
2023-12-19 12:33:39 -0800

    part6

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,9 +2,15 @@ extends Node2D
 
 @onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
 @onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+@onready var level_completed = $CanvasLayer/LevelCompleted
 
 
 func _ready():
 	#Visual Polygon appearance
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	polygon_2d.polygon =  collision_polygon_2d.polygon
+	Events.level_completed.connect(show_level_completed)
+	
+func show_level_completed():
+	level_completed.show()
+	get_tree().paused = true
