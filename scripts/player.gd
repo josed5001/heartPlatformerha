93465c27031cf9724ae32e0d4b12591788fd18f5
josed5001 <122053779+josed5001@users.noreply.github.com>
2024-02-07 22:19:41 -0800

    Part 13 + 14

--- scripts/player.gd
+++ scripts/player.gd
@@ -26,7 +26,6 @@ func _physics_process(delta):
 	handle_air_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	apply_air_resistance(input_axis, delta)
-	update_animations(input_axis)
 	var was_on_floor = is_on_floor()
 	var was_on_wall = is_on_wall_only()
 	if was_on_wall:
@@ -39,6 +38,7 @@ func _physics_process(delta):
 	var just_left_wall = was_on_wall and not is_on_wall()
 	if just_left_wall:
 		wall_jump_timer.start()
+	update_animations(input_axis)
 
 func apply_gravity(delta):
 	if not is_on_floor():
@@ -62,8 +62,9 @@ func handle_jump():
 	if is_on_floor(): air_jump =  true
 	
 	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
-		if Input.is_action_just_pressed("action_space"):
+		if Input.is_action_pressed("action_space"):
 			velocity.y = movement_data.jump_velocity
+			coyote_jump_timer.stop()
 	elif not is_on_floor():
 		if Input.is_action_just_released("action_space") and velocity.y < movement_data.jump_velocity /2:
 			velocity.y = movement_data.jump_velocity /2
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,20 +1,51 @@
 extends Node2D
 
+var level_time = 0.0
+var start_level_msec = 0.0
 @export var next_level: PackedScene
 @onready var level_completed = $CanvasLayer/LevelCompleted
+@onready var start_in = %StartIn
+@onready var start_in_label = %StartInLabel
+@onready var animation_player = $AnimationPlayer
+@onready var level_time_label = %LevelTimeLabel
 
 
 func _ready():
-	#Visual Polygon appearance
 	Events.level_completed.connect(show_level_completed)
-	
-func show_level_completed():
-	level_completed.show()
 	get_tree().paused = true
-	await get_tree().create_timer(1.0).timeout
+	start_in.visible = true
+	await LevelTransition.fade_from_black()
+	animation_player.play("countdown")
+	await animation_player.animation_finished
+	get_tree().paused = false
+	start_in.visible = false
+	start_level_msec = Time.get_ticks_msec()
+	print(start_level_msec)
+
+func _process(delta):
+	level_time = Time.get_ticks_msec() - start_level_msec
+	level_time_label.text = str(level_time / 1000.0)
+
+func retry():
+	await LevelTransition.fade_to_black()
+	get_tree().paused = false
+	get_tree().change_scene_to_file(scene_file_path)
+	
+
+func go_to_next_level():
 	if not next_level is PackedScene: return
 	await LevelTransition.fade_to_black()
 	get_tree().paused = false
 	get_tree().change_scene_to_packed(next_level)
-	LevelTransition.fade_from_black()
+	
+func show_level_completed():
+	level_completed.show()
+	level_completed.retry_button.grab_focus()
+	get_tree().paused = true
+	
 
+func _on_level_completed_next_level():
+	go_to_next_level()
+
+func _on_level_completed_retry():
+	retry()
